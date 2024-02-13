00818012310eeebee158ae2287a80f74efcfacb4
Nastyaknu <zimanastya@knu.ua>
2024-02-13 21:20:26 +0200

    Changed utils.py(1)

--- utils.py
+++ utils.py
@@ -3,3 +3,23 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+    
+def simple(n):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+num = int(input())
+if simple(num):
+    print("є простим числом")
+else:
+    print("не є простим числом")
