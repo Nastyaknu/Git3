4f514b21ac3396a8ab0accff9d588aad29a0f484
Marusi1a <marusiachasodiyka@gmail.com>
2024-02-14 22:00:29 +0200

    виклик функції нсд

--- main.py
+++ main.py
@@ -3,3 +3,13 @@ from utils.py import factorial
 num=int(imput())
 print(factorial(num))
 
+
+from utils.py import find_gcd
+
+num1 = int(input("Введіть перше число: "))
+num2 = int(input("Введіть друге число: "))
+
+gcd = find_gcd(num1, num2)
+
+print(f"НСД({num1}, {num2}) = {gcd}
+
