8ed5c9db72dffb53c2d23793e13a712a47890b24
Nastyaknu <zimanastya@knu.ua>
2024-02-14 21:58:08 +0200

    adding file main.py

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ from utils.py import factorial
 num=int(imput())
 print(factorial(num))
 
+from utils.py import  simple
+
+num = int(input())
+if simple(num):
+    print("є простим числом")
+else:
+    print("не є простим числом")
